cf36e0aaa092ee963294815a08f35cbcfb78bc66
Pradeep Kumar <dpradeep42@gmail.com>
2021-10-27 23:13:31 +0530

    Added Do While loop

---
+++ Assignment/Loops_DoWhile.java
@@ -0,0 +1,17 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Loops_DoWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        do{
+            System.out.println(n);
+            n--;
+        }
+        while(n > 0);
+        sc.close();
+    }
+}
