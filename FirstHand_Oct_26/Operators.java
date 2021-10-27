f725331b8da4625e76ec10fd59869f443bc10b47
Pradeep Kumar <dpradeep42@gmail.com>
2021-10-27 22:35:04 +0530

    Updated Operators

--- FirstHand_Oct_26/Operators.java
+++ FirstHand_Oct_26/Operators.java
@@ -2,6 +2,10 @@ package FirstHand_Oct_26;
 
 public class Operators {
     public static void main(String[] args) {
-        
+
+        //Addition +
+        int x = 5;
+        int y = 3;
+        System.out.println(x + y);
     }
 }
