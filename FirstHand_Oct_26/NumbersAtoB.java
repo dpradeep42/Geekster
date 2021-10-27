fda7d761fb694603c344f01bbe1bd9c146fb4123
Pradeep Kumar <dpradeep42@gmail.com>
2021-10-27 22:48:48 +0530

    Added numbers from A to B

---
+++ FirstHand_Oct_26/NumbersAtoB.java
@@ -0,0 +1,24 @@
+package FirstHand_Oct_26;
+
+import java.util.Scanner;
+
+public class NumbersAtoB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number : ");
+        int b = sc.nextInt();
+
+        if (b > a) {
+            for (int i = a; i <= b; i++) {
+                System.out.println(i);
+            }
+        } else {
+            for (int i = a; i >= b; i--) {
+                System.out.println(i);
+            }
+        }
+    }
+}
