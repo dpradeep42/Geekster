c01635f7b02caf699997588589883f0e82cbf902
Pradeep Kumar <dpradeep42@gmail.com>
2021-10-27 23:36:07 +0530

    Added Number of Digits in a number

---
+++ Assignment/NumberOfDigitsInNumber.java
@@ -0,0 +1,19 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class NumberOfDigitsInNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        int count = 0;
+        int temp = num;
+
+        while(num > 0){
+            count += 1;
+            num /= 10;
+        }
+        System.out.println("Number of Digits in " + temp + " is " + count);
+    }
+}
