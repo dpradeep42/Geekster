cd8dd9f8574ea17c8d9f00b8395077f1004488f8
Pradeep Kumar <dpradeep42@gmail.com>
2021-11-09 18:36:48 +0530

    Added assignment 09 Nov 2021

---
+++ Nov_09_2021/Assignment.java
@@ -0,0 +1,16 @@
+package Nov_09_2021;
+
+public class Assignment {
+    public static void main(String[] args) {
+        int n = 1;
+        while(n <= 10){
+            System.out.println(n);
+            n++;
+        }
+        System.out.println("\n");
+        while(n > 1){
+            System.out.println(n - 1);
+            n--;
+        }
+    }
+}
