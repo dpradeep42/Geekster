b983a7b1ab1ed5c9afe4c85c4cc32b0edf55a16e
Pradeep Kumar <dpradeep42@gmail.com>
2021-11-09 21:29:10 +0530

    Updated 10 Nov 2021

---
+++ Nov_10_2021/Class_2.java
@@ -0,0 +1,14 @@
+package Nov_10_2021;
+
+import java.util.Scanner;
+
+public class Class_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter your Name : ");
+        String myInput = sc.nextLine();
+        
+        System.out.println(myInput);
+    }
+}
