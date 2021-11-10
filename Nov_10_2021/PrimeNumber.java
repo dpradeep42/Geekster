3f1396e1b52e20aa9fad5b76e816f8ce44512a73
Pradeep Kumar <dpradeep42@gmail.com>
2021-11-10 16:57:00 +0530

    Updated 10 Nov 2021

--- Nov_10_2021/PrimeNumber.java
+++ Nov_10_2021/PrimeNumber.java
@@ -2,8 +2,6 @@ package Nov_10_2021;
 
 import java.util.Scanner;
 
-import javax.lang.model.util.ElementScanner14;
-
 public class PrimeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
