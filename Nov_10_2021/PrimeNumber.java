2bafa7f3c6fa33150474d0a602f20d6ae759401f
Pradeep Kumar <dpradeep42@gmail.com>
2021-11-10 16:54:44 +0530

    Updated 10 Nov 2021

---
+++ Nov_10_2021/PrimeNumber.java
@@ -0,0 +1,28 @@
+package Nov_10_2021;
+
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner14;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int n = sc.nextInt();
+        if(n <= 1)
+            System.out.println("Not a Prime");
+        else if(n == 2)
+            System.out.println("Is a Prime");
+        else if(n % 2 == 0)
+            System.out.println("Not a Prime");
+        else{
+            for(int i = 3; i <= Math.sqrt(n); i+= 2){
+                if(n % i == 0)
+                    System.out.println("Not a Prime");
+                else
+                    System.out.println("Is a Prime");
+            }
+        }
+        sc.close();
+    }
+}
