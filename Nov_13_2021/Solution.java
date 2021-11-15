1aac548c8143ec2347c3a7c421cecff1c46fce54
Pradeep Kumar <dpradeep42@gmail.com>
2021-11-15 22:16:51 +0530

    Added 15 Nov 2021

---
+++ Nov_13_2021/Solution.java
@@ -0,0 +1,43 @@
+package Nov_13_2021;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int basic = sc.nextInt();
+        char grade = sc.next().charAt(0);
+        if (basic == 0)
+            System.out.println("0");
+        else
+            System.out.println(computeSalary(basic, grade));
+        sc.close();
+    }
+
+    static int computeSalary(int basic, char grade) {
+        double allowance;
+        double hra, da, pf;
+
+        hra = 0.2 * basic;
+        da = 0.5 * basic;
+        pf = 0.11 * basic;
+
+        // Condition to compute the
+        // allowance for the person
+        if (grade == 'A') {
+            allowance = 1700.0;
+        } else if (grade == 'B') {
+            allowance = 1500.0;
+        } else {
+            allowance = 1300.0;
+        }
+        double gross;
+
+        // Calculate gross salary
+        gross = Math.round(basic + hra + da + allowance - pf);
+
+        return (int) gross;
+
+    }
+}
