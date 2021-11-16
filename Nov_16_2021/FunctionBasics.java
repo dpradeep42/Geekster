324c42e8a7fa791eea5a61317736b7edeb933360
Pradeep Kumar <dpradeep42@gmail.com>
2021-11-16 21:14:56 +0530

    Updated 16 Nov 2021

--- Nov_16_2021/FunctionBasics.java
+++ Nov_16_2021/FunctionBasics.java
@@ -3,9 +3,9 @@ package Nov_16_2021;
 import java.util.Scanner;
 
 public class FunctionBasics {
-    public static int factorial(int n){
+    public static int factorial(int n) {
         int fact = 1;
-        for(int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) {
             fact *= i;
         }
         return fact;
---
+++ Nov_16_2021/name_function.java
@@ -0,0 +1,18 @@
+package Nov_16_2021;
+
+import java.util.Scanner;
+
+public class name_function {
+    public static String addNames(String fn, String ln){
+        return fn + " " + ln;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String firstName = sc.next();
+        String lastName = sc.next();
+        System.out.println("Your full name is " + addNames(firstName, lastName));
+        sc.close();
+    }
+}
