1d0655ef2d6fa63febd11eb4af9e44bd1dc10642
Pradeep Kumar <dpradeep42@gmail.com>
2021-11-19 22:37:14 +0530

    Added Assignment 16 Nov 2021

---
+++ Assignment_16_Nov_2021/FactorialLoop.java
@@ -0,0 +1,28 @@
+package Assignment_16_Nov_2021;
+
+import java.util.Scanner;
+
+public class FactorialLoop {
+
+    static long factorial(int num){
+        return (num == 1 || num == 0) ? 1 : num * factorial(num - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char input = 'Y';
+
+        while(input == 'Y' || input == 'y'){
+            System.out.print("Enter a number : ");
+            int n = sc.nextInt();
+
+            System.out.println("Factorial of " + n + " is " + factorial(n) + ".");
+
+            System.out.print("Do you want to continue (Y/N) : ");
+            input = sc.next().charAt(0);
+        }
+
+        sc.close();
+    }
+}
---
+++ Assignment_16_Nov_2021/NumberOfDigitsInANumber.java
@@ -0,0 +1,19 @@
+package Assignment_16_Nov_2021;
+
+import java.util.Scanner;
+
+public class NumberOfDigitsInANumber {
+
+    static int numberOfDigits(int num){
+        return Integer.toString(num).length();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        System.out.println(n + " has " + numberOfDigits(n) + " digits.");
+
+        sc.close();
+    }
+}
