4f26f2155a7176d941e204e92bbebfa4e9ae77fc
Pradeep Kumar D <dpradeep42@gmail.com>
2021-12-15 19:41:28 +0530

    Added Nov 24 Assignment

---
+++ Assignment_24_Nov_2021/FirstUpper.java
@@ -0,0 +1,29 @@
+package Assignment_24_Nov_2021;
+
+import java.util.Scanner;
+
+public class FirstUpper {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = "";
+        System.out.println("Enter the sentence :");
+        str = sc.nextLine();
+        char[] arr = str.toCharArray();
+        String s = "";
+        s = s + arr[0];
+        s = s.toUpperCase();
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != ' ') {
+                s = s + arr[i];
+            } else if (arr[i] == ' ') {
+                String up = "";
+                up = up + arr[i + 1];
+                up = up.toUpperCase();
+                s = s + " " + up;
+                i++;
+            }
+        }
+        System.out.println("Modified String :\n" + s);
+        sc.close();
+    }
+}
---
+++ Assignment_24_Nov_2021/WordSearch.java
@@ -0,0 +1,20 @@
+package Assignment_24_Nov_2021;
+
+import java.util.Scanner;
+
+public class WordSearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = "";
+        System.out.println("Enter the sentence :");
+        str = sc.nextLine();
+        String search = "";
+        System.out.println("Enter the word to be searched : (Remember Java is Case Sensitive!!)");
+        search = sc.nextLine();
+        int idx = str.indexOf(search);
+        if (idx == -1)
+            System.out.println("Word not found!");
+        else
+            System.out.println("Word found at index : " + idx);
+    }
+}
